6255840cf342e349510c6cfc0372f09bce3633a6
HarshvardhanShringi <81759722+HarshvardhanShringi@users.noreply.github.com>
2022-07-08 21:38:41 +0530

    Update 1.Explore your data.py

--- Tasks/Prerequisties/ML Introduction/1.Explore your data.py
+++ Tasks/Prerequisties/ML Introduction/1.Explore your data.py
@@ -1,1 +1,20 @@
+from learntools.core import binder
+binder.bind(globals())
+from learntools.machine_learning.ex2 import *
+
+import pandas as pd
+
+# Path of the file to read
+iowa_file_path = '../input/home-data-for-ml-course/train.csv'
+
+# Fill in the line below to read the file into a variable home_data
+home_data = pd.read_csv(iowa_file_path)
+
+
+home_data.describe()
+
+avg_lot_size = 10517
+
+# As of today, how old is the newest home (current year - the date in which it was built)
+newest_home_age = 10
 
