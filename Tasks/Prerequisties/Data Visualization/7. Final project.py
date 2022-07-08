a4dd3994fb471e46307381cf128a3313794648e1
HarshvardhanShringi <81759722+HarshvardhanShringi@users.noreply.github.com>
2022-07-08 17:46:52 +0530

    Update 7. Final project.py

--- Tasks/Prerequisties/Data Visualization/7. Final project.py
+++ Tasks/Prerequisties/Data Visualization/7. Final project.py
@@ -1,1 +1,16 @@
+# importing libraries
+import pandas as pd
+pd.plotting.register_matplotlib_converters()
+import matplotlib.pyplot as plt
+%matplotlib inline
+import seaborn as sns
+from learntools.core import binder
+binder.bind(globals())
+from learntools.data_viz_to_coder.ex7 import *
 
+# Loading data
+my_filepath = "../input/fivethirtyeight-comic-characters-dataset/dc-wikia-data.csv"
+my_data = pd.read_csv(my_filepath)
+
+# making barplot
+sns.barplot(data=my_data)
