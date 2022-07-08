0367dcdb48f3d9905a8dc772bb76c54ea951c28d
HarshvardhanShringi <81759722+HarshvardhanShringi@users.noreply.github.com>
2022-07-08 16:46:38 +0530

    Update 4. Character encoding .py

--- Tasks/Prerequisties/Data Cleaning/4. Character encoding .py
+++ Tasks/Prerequisties/Data Cleaning/4. Character encoding .py
@@ -1,1 +1,20 @@
+from learntools.core import binder
+binder.bind(globals())
+from learntools.data_cleaning.ex4 import *
+import pandas as pd
+import numpy as np
+import chardet
 
+# 1. Encodindg
+sample_entry = b'\xa7A\xa6n'
+print(sample_entry)
+print('data type:', type(sample_entry))
+before = sample_entry.decode("big5-tw")
+new_entry = before.encode()
+
+
+# 2. Reading files with encoding problems
+police_killings = pd.read_csv("../input/fatal-police-shootings-in-the-us/PoliceKillingsUS.csv", encoding='Windows-1252')
+
+# 3. Saving with UTF 8
+police_killings.to_csv("my_file.csv")
