42003e1fe7e52b6ecd9a3ffae3690399a171789c
HarshvardhanShringi <81759722+HarshvardhanShringi@users.noreply.github.com>
2022-07-08 17:17:18 +0530

    Update 1. Hello seaborn.py

--- Tasks/Prerequisties/Data Visualization/1. Hello seaborn.py
+++ Tasks/Prerequisties/Data Visualization/1. Hello seaborn.py
@@ -1,1 +1,19 @@
+# importing libraries
+import pandas as pd
+pd.plotting.register_matplotlib_converters()
+import matplotlib.pyplot as plt
+%matplotlib inline
+import seaborn as sns
+import os
+if not os.path.exists("../input/fifa.csv"):
+    os.symlink("../input/data-for-datavis/fifa.csv", "../input/fifa.csv")  
+from learntools.core import binder
+binder.bind(globals())
+from learntools.data_viz_to_coder.ex1 import *
 
+#1. Loading data
+fifa_filepath = "../input/fifa.csv"
+fifa_data = pd.read_csv(fifa_filepath, index_col="Date", parse_dates=True)
+
+#2. Plotting
+sns.lineplot(data=fifa_data)
